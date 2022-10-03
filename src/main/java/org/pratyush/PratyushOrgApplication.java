09719a4a7308074442984e310da444c608b73aa5
Pratyush Choudhary <solixious@gmail.com>
2022-10-03 20:59:11 +0400

    Initial Commit

---
+++ src/main/java/org/pratyush/PratyushOrgApplication.java
@@ -0,0 +1,13 @@
+package org.pratyush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PratyushOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PratyushOrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pratyush/PratyushOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pratyush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PratyushOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
